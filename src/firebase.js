88831c0041d8b61ac6bd820f102985692b57e06e
Prathibha Ratnayake <prsthibha@gmail.com>
2022-10-26 23:27:45 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlNeRq6ixxYhFyl_rgTF5PbFOgkuksaDc",
+  authDomain: "netflix-clone-c1b3f.firebaseapp.com",
+  projectId: "netflix-clone-c1b3f",
+  storageBucket: "netflix-clone-c1b3f.appspot.com",
+  messagingSenderId: "1062621980365",
+  appId: "1:1062621980365:web:53f168e31f95d3a2acab84"
+};
+
+
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import { Nav } from '../../components'
+import './HomeScreen.css'
+
+const HomeScreen = () => {
+    return (
+        <div className='homeScreen'>
+            <Nav />
+            {/* banner */}
+            {/* row */}
+        </div>
+    )
+}
+
+export default HomeScreen
